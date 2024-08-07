8fa306ccb546d7b6669a99a7d87c046a15f6d7c8
piu2121 <elias71802@gmail.com>
2024-08-07 18:52:04 -0300

    trabalho Luís,Weber,Marcos

---
+++ familia amigo/assets/main.js
@@ -0,0 +1,72 @@
+document.querySelector(".div5 img").addEventListener("mouseover", () => {
+    sovio.play()
+})
+
+document.querySelector(".div5 img").addEventListener("mouseout", () => {
+    sovio.pause()
+})
+
+document.querySelector(".div2 img").addEventListener("mouseover", () => {
+    fogo.play()
+})
+
+document.querySelector(".div2 img").addEventListener("mouseout", () => {
+    fogo.pause()
+})
+
+document.querySelector(".div3 img").addEventListener("mouseover", () => {
+    shin.play()
+})
+
+document.querySelector(".div3 img").addEventListener("mouseout", () => {
+    shin.pause()
+})
+
+document.querySelector(".div4 img").addEventListener("mouseover", () => {
+    monologo.play()
+})
+
+document.querySelector(".div4 img").addEventListener("mouseout", () => {
+    monologo.pause()
+})
+
+document.querySelector(".div6 img").addEventListener("mouseover", () => {
+    devilgun.play()
+})
+
+document.querySelector(".div6 img").addEventListener("mouseout", () => {
+    devilgun.pause()
+})
+
+document.querySelector(".div7 img").addEventListener("mouseover", () => {
+    reidechifres.play()
+})
+
+document.querySelector(".div7 img").addEventListener("mouseout", () => {
+    reidechifres.pause()
+})
+
+document.querySelector(".div10 img").addEventListener("mouseover", () => {
+    fracassado.play()
+})
+
+document.querySelector(".div10 img").addEventListener("mouseout", () => {
+    fracassado.pause()
+})
+
+document.querySelector(".nascimento img").addEventListener("mouseover", () => {
+    sovio.play()
+})
+
+document.querySelector(".nascimento img").addEventListener("mouseout", () => {
+    sovio.pause()
+})
+let=tanjitina
+let=ponja
+let=fin 
+let=denje
+let=ma1
+let=ma2
+let=ma3
+let=ma4
+ma4.addEventListener(".adventuretime")    
